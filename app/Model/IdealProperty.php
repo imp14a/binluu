4a36b633444fb507c61f4b572528970aae56a8f8
Agustin Ramirez <im1p4a@gmail.com>
2013-10-31 09:26:54 -0600

    Person register

--- app/Model/IdealProperty.php
+++ app/Model/IdealProperty.php
@@ -3,6 +3,17 @@
 class IdealProperty extends AppModel {
 
 	public $name = 'IdealProperty';
+
+	public $validate = array(
+		'latitude' => array(
+			'required' => array('rule' => array('minLength', '1'),
+				'message' => 'Necesitas seleccionar una ubicación')
+			),
+		'longitude'   => array(
+			'required' => array('rule' => array('minLength', '1'),
+				'message' => 'Necesitas seleccionar una ubicación')
+			)
+		);
 	
 }
 
